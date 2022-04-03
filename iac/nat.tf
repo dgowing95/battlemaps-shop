8a1aad96af69f9fb635fe350fad2955a6bf568c2
dgowing95 <danielgowing95@gmail.com>
2022-04-03 22:36:45 +0300

    Allow DNS egress

--- iac/nat.tf
+++ iac/nat.tf
@@ -98,6 +98,15 @@ resource "aws_security_group_rule" "egress_https" {
     cidr_blocks = ["0.0.0.0/0"]
     security_group_id = aws_security_group.dmz.id
 }
+resource "aws_security_group_rule" "egress_dns" {
+    type = "egress"
+    description = "Allow DNS egress"
+    from_port = 53
+    to_port = 53
+    protocol = "UDP"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.dmz.id
+}
 
 resource "aws_iam_role" "dmz_role" {
     name = "dmz-role"
