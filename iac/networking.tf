cd65c5e2a9190115cc93f6e5a4cf8b51e9bd0f62
dgowing95 <danielgowing95@gmail.com>
2022-04-03 19:29:27 +0300

    naming changes and fix subnet overlap

--- iac/networking.tf
+++ iac/networking.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "vpc" {
   cidr_block = "30.0.0.0/16"
   tags = {
-      Name = "Battlemap VPC"
+      Name = "iac_vpc"
   }
 }
 
@@ -25,7 +25,7 @@ resource "aws_subnet" "public" {
   count = length(data.aws_availability_zones.azs.names)
 
   vpc_id     = aws_vpc.vpc.id
-  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 8, (length(data.aws_availability_zones.azs.names) - 1) + count.index)
+  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 8, length(data.aws_availability_zones.azs.names) + count.index)
 
   tags = {
     Name = "pub_subnet_${count.index}"
