3836861a09507c745eb1a54b6822fb9568ec55e2
dgowing95 <danielgowing95@gmail.com>
2022-04-03 21:54:56 +0300

    Add instance profile to launch template

--- iac/nat.tf
+++ iac/nat.tf
@@ -30,7 +30,7 @@ resource "aws_launch_template" "dmz" {
     instance_type = var.nat_instance
     key_name = aws_key_pair.key.key_name
     iam_instance_profile {
-        name = aws_iam_role.dmz_role.name
+        name = aws_iam_instance_profle.dmz_profile.name
     }
     network_interfaces {
         associate_public_ip_address = true
@@ -104,4 +104,9 @@ resource "aws_iam_role" "dmz_role" {
         ]
         })
     }
+}
+
+resource "aws_iam_instance_profle" "dmz_profile" {
+    name = "dmz_profile"
+    role = aws_iam_role.dmz_role.name
 }
