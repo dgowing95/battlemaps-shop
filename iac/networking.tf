b8cf3ed66f65a6960710fc9a3961e42d11acc50a
dgowing95 <danielgowing95@gmail.com>
2022-04-03 19:37:36 +0300

    Add IGW and public route tables

--- iac/networking.tf
+++ iac/networking.tf
@@ -31,3 +31,27 @@ resource "aws_subnet" "public" {
     Name = "pub_subnet_${count.index}"
   }
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "iac_igw"
+  }
+}
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+}
+
+resource "aws_route_table_association" "assoc" {
+  for_each = aws_subnet.public
+  subnet_id = each.value.id
+  route_table_id = aws_route_table.public.id
+}
