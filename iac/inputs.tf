665f83211f60e7e355d7adee0e4d519aecf03e73
dgowing95 <danielgowing95@gmail.com>
2022-04-03 19:21:58 +0300

    Add initial terraform configs

---
+++ iac/inputs.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  type = string
+}
+variable "access_key" {
+  type      = string
+  sensitive = true
+}
+variable "secret_key" {
+  type      = string
+  sensitive = true
+}
---
+++ iac/networking.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+data "aws_availability_zones" "azs" {
+  all_availability_zones = true
+}
+
+resource "aws_subnet" "private" {
+  count = length(data.aws_availability_zones.azs.names)
+
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 8, count.index)
+
+  tags = {
+    Name = "priv_subnet_${count.index}"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(data.aws_availability_zones.azs.names)
+
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = cidrsubnet(aws_vpc.vpc.cidr_block, 8, (length(data.aws_availability_zones.azs.names) - 1) + count.index)
+
+  tags = {
+    Name = "pub_subnet_${count.index}"
+  }
+}
