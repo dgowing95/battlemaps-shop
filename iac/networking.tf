73760677838cfb0062c341b895e21d85f45bb964
dgowing95 <danielgowing95@gmail.com>
2022-04-03 19:41:53 +0300

    Fix for_each error

--- iac/networking.tf
+++ iac/networking.tf
@@ -51,7 +51,8 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table_association" "assoc" {
-  for_each = aws_subnet.public
-  subnet_id = each.value.id
+  count = length(aws_subnet.public)
+
+  subnet_id = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
