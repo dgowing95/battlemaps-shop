b0ce54b33fb8a2d8ea07abe57d201215337a1231
dgowing95 <danielgowing95@gmail.com>
2022-04-03 21:28:47 +0300

    .

--- iac/nat.tf
+++ iac/nat.tf
@@ -74,7 +74,7 @@ resource "aws_security_group" "dmz" {
 }
 
 resource "aws_iam_role" "dmz_role" {
-    name = "dmz_role"
+    name = "dmz-role"
 
     assume_role_policy = jsonencode({
     Version = "2012-10-17"
