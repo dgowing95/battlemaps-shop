8a7b181aedf7c83bae9c8bd85952abf5fe54743f
dgowing95 <danielgowing95@gmail.com>
2022-04-04 20:59:44 +0300

    .

--- iac/nat.tf
+++ iac/nat.tf
@@ -4,6 +4,8 @@ resource "aws_autoscaling_group" "dmz" {
     max_size = 1
     desired_capacity = 1
     health_check_grace_period = 300
+    availability_zones = data.aws_availability_zones.azs.zone_ids.0
+
 
     launch_template {
         id = aws_launch_template.dmz.id
