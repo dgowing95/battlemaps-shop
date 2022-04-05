66c05f2c89299472e96fcf77b11b8fb26caaa1ee
dgowing95 <danielgowing95@gmail.com>
2022-04-05 22:41:17 +0300

    RDS rename and enable backups

--- iac/rds.tf
+++ iac/rds.tf
@@ -5,11 +5,15 @@ resource "aws_db_instance" "main" {
   instance_class = "db.t3.micro"
   name = "orion"
   username = "root"
+  identifier = "orion"
   password = var.rds_root_password
   vpc_security_group_ids = [
       aws_security_group.db.id
   ]
   db_subnet_group_name = aws_db_subnet_group.main.name
+  backup_retention_period = 14
+  backup_window = "04:00-06:00"
+  maintenance_window = "Mon:00:00-Mon:02:30"
 }
 
 resource "aws_db_subnet_group" "main" {
