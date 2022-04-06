3b8c1c48a0c45fe99b8777488bb086936bf78ccc
dgowing95 <danielgowing95@gmail.com>
2022-04-06 21:39:45 +0300

    .

--- iac/cloudflare.tf
+++ iac/cloudflare.tf
@@ -3,22 +3,22 @@ resource "cloudflare_zone" "battlemaps" {
     plan = "free"
 }
 
-# resource "cloudflare_record" "acm_validation" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       value = dvo.resource_record_value
-#       type   = dvo.resource_record_type
+resource "cloudflare_record" "acm_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      value = dvo.resource_record_value
+      type   = dvo.resource_record_type
       
-#     }
-#   }
-#   zone_id = cloudflare_zone.battlemaps.id
-#   name = each.value.name
-#   value = trimsuffix(each.value.value, ".")
-#   type = each.value.type
-#   proxied = false
+    }
+  }
+  zone_id = cloudflare_zone.battlemaps.id
+  name = each.value.name
+  value = trimsuffix(each.value.value, ".")
+  type = each.value.type
+  proxied = false
 
-# }
+}
 resource "cloudflare_record" "domain" {
     zone_id = cloudflare_zone.battlemaps.id
     name = var.site_url
